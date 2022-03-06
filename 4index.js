51743ada7f4716d582d4a19a4faeece9ea40ae06
MOHAMMED-BALWA <79078999+MOHAMMED-BALWA@users.noreply.github.com>
2022-03-06 16:53:55 +0530

    Add files via upload

---
+++ 4index.js
@@ -0,0 +1,7 @@
+function back(){
+    window.location = "index.html";
+}
+function order()
+{
+    document.getElementById("fff").innerHTML ="YOUR ORDER HAS BEEN PLACED"
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+function bestseller(){
+    window.location = "2index.html";
+}
+
+
+function menu(){
+    window.location = "3index.html";
+}
+
+
+function fform(){
+    window.location = "4index.html";
+}
